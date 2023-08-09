25528beff9e1e1d6892f7c6e2a739b83419e64ea
Luis <LuisDEsquivel11@gmail.com>
2023-08-09 15:10:41 -0500

    added hero

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,22 @@
 "use strict"
 
+function compareTriplets(a, b) {
+    let aScore = 0;
+    let bScore = 0;
+    let scores = []
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            aScore++;
+        } else if (a[i] < b[i]) {
+            bScore++;
+        } else {
+
+        }
+    }
+    scores.push(aScore);
+    scores.push(bScore);
+    return scores;
+}
+let a = [1, 2, 3]
+let b = [3, 2, 1]
+console.log(compareTriplets(a, b))
