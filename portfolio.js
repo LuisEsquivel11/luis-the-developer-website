5657e78a069b91903f151dbadf1e7bb8aac09322
Luis <LuisDEsquivel11@gmail.com>
2023-08-14 16:43:36 -0500

    Event listeners added to nav bar items

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,48 @@
+(function() {
 "use strict"
 
+//-----------------------------------Nav Bar ---------------------------------------//
+const dropdowns = document.querySelectorAll('li.parent');
+for(let dropdown of dropdowns) {
+    let toggle = dropdown.querySelector('.toggle');
+    toggle.addEventListener('click', ()=>{
+        dropdown.classList.toggle('show');
+    });
+}
+const contactLink = document.querySelector('li.contact');
+const contactModal = document.querySelector('div.modal');
+const modalBackground = document.querySelector('div.modal-bg');
+const hero = document.querySelector('.hero');
+contactLink.addEventListener('click', () => {
+    contactModal.style.zIndex = 999;
+});
+modalBackground.addEventListener('click', ()=> {
+    contactModal.style.zIndex = -5;
+});
+    document.addEventListener("DOMContentLoaded", () => {
+        const navbarItems = document.querySelector(".nav-item");
+        navbarItems.classList.;
+    });
+
+
+
+
+
+
+
+
+//-----------------------------------Nav Bar ---------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
